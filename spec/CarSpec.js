cec5dd34f58c9ff8ccbb86afe5b7f8f46c649576
Gilbert <gilbertbgarza@gmail.com>
2014-02-25 14:13:56 -0600

    Remove cruft specs

--- spec/CarSpec.js
+++ spec/CarSpec.js
@@ -1,12 +1,4 @@
-describe("Player", function() {
-
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
+describe("Car", function() {
 
   describe("A car with a driver", function() {
     var car;
--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -1,4 +1,4 @@
-describe("Player", function() {
+describe("Person", function() {
   var person;
 
   it("should have a name", function() {
