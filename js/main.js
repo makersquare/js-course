ee4d3a176a6366de3f203c0a963097b7d9fa939f
Osei Bonsu <osei.bonsu@gmail.com>
2014-04-08 14:43:27 -0500

    Create music video lesson branch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert('hi');
+
+var videos = [
+  { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
+  { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
+];
+
+var renderVideoList = function () {
+  // TODO
+};
