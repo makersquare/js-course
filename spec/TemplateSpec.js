f1fdec981c8a945a6bb0dd5d8e28c34fe9d9901f
Osei Bonsu <osei.bonsu@gmail.com>
2014-04-08 12:38:19 -0500

    Add template tests

--- spec/TemplateSpec.js
+++ spec/TemplateSpec.js
@@ -1,10 +1,12 @@
 describe("Render Function for Templates", function() {
 
-  var template, person, person2;
+  var template, template2, person, person2;
 
   beforeEach(function() {
 
     template = "<div class='firstName'>{{firstName}}</div>";
+    template2 = "<div class='firstName'>{{firstName}}{{firstName}}</div>";
+    template3 = "<div class='firstName'>{{firstName}} {{lastName}}</div>";
 
     person = {
       firstName: "Gilbert",
@@ -18,8 +20,30 @@ describe("Render Function for Templates", function() {
 
   });
 
-  it("Should replace the interpolate values of template with object properties of the same name", function() {
+  it("Should be a function", function() {
+    expect(render instanceof Function).toBe(true);
+  });
+
+  it("Should return a string", function() {
     var result = render(template, person);
+    expect(typeof result ).toBe("string");
+  });
+
+  it("Should interpolate values of template with object properties of the same name", function() {
+    var result = render(template, person);
+    expect(result).toBe("<div class='firstName'>Gilbert</div>");
+  });
+
+  it("Should interpolate multiple values of template with object properties of the same name", function() {
+    var result = render(template2, person);
+    expect(result).toBe("<div class='firstName'>GilbertGilbert</div>");
+  });
+
+  it("Should interpolate different values of template with object properties of the same name", function() {
+    var result = render(template3, person);
+    expect(result).toBe("<div class='firstName'>Gilbert JS</div>");
+    var result2 = render(template3, person2);
+    expect(result2).toBe("<div class='firstName'>Osei It's complicated</div>");
   });
 
 });
