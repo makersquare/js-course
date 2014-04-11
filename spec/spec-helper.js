ed687dcc396469be0368210f67add5174fa07955
Jose R. Borja <chakaitos@gmail.com>
2014-04-11 11:48:48 -0500

    Finish implementing the myQuery library - all tests pass

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,1 +1,31 @@
 // Any test helpers go here
+
+
+// /**
+//          * Hide element(s) from DOM
+//          * @returns {*}
+//          */
+//         hide: function () {
+//             var len = this.length;
+//             // Here we simply loop through our object (this) and set the css to display none. 
+//             //If you got more that 1 node from DOM selected with querySelectorAll, you would hide them all.
+//             while (len--) {
+//                 this[len].style.display = 'none';
+//             }
+ 
+//             // It's important to return this if you want to chain methods!
+//             return this;
+//         },
+ 
+//         /**
+//          * Show element(s) from DOM
+//          * @returns {*}
+//          */
+//         show: function () {
+//             var len = this.length;
+//             while (len--) {
+//                 this[len].style.display = 'block';
+//             }
+ 
+//             return this;
+//         }    
--- src/myquery.js
+++ src/myquery.js
@@ -2,12 +2,97 @@
 
   var QueryWrapper = function (elems) {
     // TODO
+    this.get = function(index) {
+		  return elems[index];
+		};
+    this.length = elems.length;
+    this.each = function(func) {
+		  for(var i = 0; i < elems.length; i++) {
+		    func(elems[i], i);
+	  	}
+	  };
+	  this.hide = function() {
+	  	this.each(function(elems) {
+		    elems.style.display = "none";
+      });
+      return this;
+	  };
+    this.show = function() {
+      this.each(function(elems) {
+        elems.style.display = "block";
+      });
+      return this;
+    };
+    this.addClass = function(className) {
+      this.each(function(elems) {
+        elems.className += className;
+      });
+      return this;
+    };
+    this.css = function(object, property) {
+       
+       // if (arguments.length === 2) {
+      if (typeof object == 'string' || object instanceof String) {
+        this.each(function(elems) {
+          elems.style[object] = property;
+        });
+      } else {
+        this.each(function(elems) {
+          for (var prop in object) {
+            elems.style[prop] = object[prop]
+          }
+        });
+      };
+      return this;
+    };
   };
 
   var myQuery = function (selector) {
-    // TODO
+    if (selector[0] == '#') {
+    selector = selector.slice(1)
+    elements = [document.getElementById(selector)]
+    }
+    else if (selector[0] == '.') {
+      class_selector = selector.slice(1)
+      elements = document.getElementsByClassName(class_selector)
+    }
+    else {
+      elements = document.getElementsByTagName(selector)
+    }
+    return new QueryWrapper(elements)
   };
 
-  window.$ = myQuery;
 
+	window.$ = myQuery;
+	$.version = "beta"
+
+	$.each = function(array, func) {
+	  for(var i = 0; i < array.length; i++) {
+	    func(array[i]);
+	  }
+	};
 })();
+
+// FOR EXTENSIONS
+// 		var rexClass = 	/(\.[_a-z]+[_a-z0-9-:\\]*)/ig;
+// 		var rexId = 		/(#[a-z]+[_a-z0-9-:\\]*)/ig;
+// 		var rexTag = 		/([a-z]+[_a-z0-9-:\\]*)/ig;
+//    var elements = [];
+
+//     // for (var i = 0; i < selector.length; i++) {
+    
+//     if ( selector[i].match(rexId) == selector[i] ) {
+//     	result = document.getElementById(...);
+//     	elements.push(result);
+//     }
+//     else if (selector[i].match(rexClass) == selector[i] ) {
+//     	result = document.getElementByClassName(selector[i]);
+//     	elements.push(result);
+//     }
+//     else if (selector[i].match(rexTag) == selector[i] ) {
+//     	result = document.getElementByTagName(selector[i]);
+//     	elements.push(result);
+//     }
+    // };
+
+
