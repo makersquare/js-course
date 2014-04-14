e3cfa36bfed7868a04497dd16bbf945b4af353ab
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-14 11:50:22 -0500

    events

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,50 @@
 var Events = {
+  on: function(event, func) {
+    // on is a function that takes two parameters
+    // on adds object to inertnal events object
+    // test if that object is already in that object
+    if ( !this.events ) { // object being called does not have event
+      // add event to events object
+      // if you don't do this you will set events to be empty object
+      // everytime on is called
+      this.events = {};  // so makes an object on whaterver
+      //this.events[event] = [func]; //
+    }
+    if (!this.events[event]) { // check if events object has event
+      this.events[event] = []; // if it doesn't create event to empty array
+       // then, push in func
+    }
+    this.events[event].push(func)
+
+
+
+  },
+  trigger: function(event) {
+    // events = {}
+    // events[event] = []
+    // always be aware of what you are passing through
+
+    for(var i = 0; i < this.events[event].length;i++) {
+      this.events[event][i]();
+    }
+  }
 
 };
+
+
+// 1. dog = extend({}, Events);
+
+// 2. dog = {
+//   on: func,
+//   trigger: func
+// }
+
+// 3. dog.on("bark", func)
+
+// 4. dog = {
+//   events: {
+//     bark: []
+//   },
+//   on: func,
+//   trigger: func
+// }
--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,11 @@
-var extend = function() {
+var extend = function(destination,source) {
+  // for object all properties in source
+  // put in object destination
+  // therefore you need to iterate through source object
+  // do something in block so it is added to the destination object
+  for(var prop in source) {
+    destination[prop] = source[prop]
+  }
+  return destination;
 };
+
