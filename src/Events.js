4e905d73370c6de22f7a09e268572ea34b7044b2
Moiz Jangda <macdaddy@Moizs-MacBook-Pro.local>
2014-04-14 10:41:41 -0500

    passes all tests.

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,33 @@
 var Events = {
+  on: function(property, func) {
 
+    if (!this.events) {
+      this.events = {};
+    }
+
+    if (!this.events[property]) {
+      this.events[property] = [];
+    }
+
+    this.events[property].push(func);
+
+  },
+  trigger: function(action) {
+    for (var property in this.events) {
+      if (property == action) {
+        for (var i = 0; i < this.events[property].length; i+=1) {
+          this.events[property][i]();
+        }
+      }
+    }
+  }
 };
+
+
+$('div').on ('click', function() {
+  consoloe.log('hi');
+});
+
+$('div').on ('click', function() {
+  consoloe.log('bye');
+});
--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,9 @@
-var extend = function() {
+var extend = function(destination, source) {
+   for (var key in source) {
+      temp = source[key]
+      destination[key] = temp;
+   }
+
+   return destination;
+
 };
