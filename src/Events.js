f6ddf39ec4066f1609592696a414fcec402abb78
Brady Bryan <jbraden.bryan@gmail.com>
2014-04-14 10:53:07 -0500

    finishes the assignment

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,22 @@
 var Events = {
+  on : function(prop, funct){
 
+    if(this.events === undefined){
+      this.events = {};
+    }
+
+    if(this.events[prop] === undefined){
+      this.events[prop] = [];
+    }
+
+    this.events[prop].push(funct);
+
+  },
+  trigger : function(prop){
+    if ( this.events[prop] != undefined ) {
+    for(var i = 0; i < this.events[prop].length; i +=1){
+      this.events[prop][i]();
+    }
+  }
+  }
 };
