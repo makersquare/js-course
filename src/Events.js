13763ad365ea0d94b7066dbcc8b21b71a315948b
Wendy Hsu <wndyhsu@gmail.com>
2014-04-14 10:37:52 -0500

    completed project

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,20 @@
 var Events = {
+  on: function(str, func){
+
+    // console.log("woof");
+    if (!this.events){
+      this.events = {};
+    }
+    if(!this.events[str]){
+      this.events[str] = [];
+    }
+    this.events[str].push(func);
+  },
+  trigger: function(event){
+    for(var i=0; i< this.events[event].length; i++){
+      this.events[event][i]();
+    }
+
+  }
 
 };
