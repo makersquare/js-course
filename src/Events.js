475782fbd95cae8d840c91656d8c73f0a703c625
Brian Provost <brianmprovost@gmail.com>
2014-04-14 10:13:50 -0500

    Implement extend and events.

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,18 @@
 var Events = {
+  on: function(action, f) {
+    if(!this.events) {
+      this.events = {};
+    }
+    if (!this.events[action]) {
+      this.events[action] = [];
+    }
+    this.events[action].push(f);
 
+  },
+
+  trigger: function(action) {
+    for (var i = 0; i < this.events[action].length; i++) {
+      this.events[action][i]();
+    }
+  }
 };
