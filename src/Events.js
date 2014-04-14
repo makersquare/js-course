86109715cf79f09114f0713e8d98e2d02ce5e238
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-14 11:18:15 -0500

    events, need to go back solidfy understanding

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,48 @@
 var Events = {
+  on: function(event, func) {
+    // on is a function that takes two parameters
+    // on adds object to inertnal events object
+    // test if that object is already in that object
+    if ( !this.events ) { // object being called does not have event
+      // add event to events object
+      this.events = {};
+      //this.events[event] = [func]; //
+    }
+    if (!this.events[event]) { // check if events object has event
+      this.events[event] = []; // if it doesn't create event to empty array
+       // then, push in func
+    }
+    this.events[event].push(func)
+
+
+
+  },
+  trigger: function(event) {
+    // events = {}
+    // events[event] = []
+    // always be aware of what you are passing through
+
+    for(var i = 0; i < this.events[event].length;i++) {
+     this.events[event][i]();
+    }
+  }
 
 };
+
+// 1. dog = extend({}, Events);
+
+// 2. dog = {
+//   on: func,
+//   trigger: func
+// }
+
+// 3. dog.on("bark", func)
+
+// 4. dog = {
+//   events: {
+//     bark: []
+//   },
+//   on: func,
+//   trigger: func
+// }
+
