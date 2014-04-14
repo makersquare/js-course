6af7a91731693e8ff3058235fd6f2f95ab1d9a1d
Jose R. Borja <chakaitos@gmail.com>
2014-04-14 11:15:03 -0500

    Implement all necessary code - All tests pass

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,22 @@
 var Events = {
-
+	on: function(event, func) {
+		// this will create an object "events" if its not already created
+		if (!this.events) {
+			this.events = {};
+		};
+		// this will create a property and set it to be an empty array 
+		// for the events object if it doesn't exist already
+		if (!this.events[event]) {
+			this.events[event] = [];
+		};
+		// this will push a function into the empty array created for the events
+		// property  specified
+		this.events[event].push(func);
+	},
+	trigger: function(event) {
+		// this will loop over the array of functions and call them one by one
+    for(var i = 0; i < this.events[event].length; i++) {
+      this.events[event][i]();
+    }
+	}
 };
--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,6 @@
-var extend = function() {
+var extend = function(destination, source) {
+	for (var prop in source) {
+		destination[prop] = source[prop];
+	}
+	return destination;
 };
