03cbc6ae7a1f476764cd93156b4149363d25fe56
Christopher Palmer <snly2386@gmail.com>
2014-04-14 10:08:41 -0500

    observer/subscriber finish

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,16 @@
 var Events = {
-
+  on: function(eventname, func) {
+    if (!this.events) {
+      this.events = {}
+    }
+    if (!this.events[eventname]) {
+      this.events[eventname] = []
+    }
+    this.events[eventname].push(func)
+  }, 
+  trigger: function(event) {
+    for( var i = 0; i < this.events[event].length; i++) {
+      this.events[event][i]();
+    }
+  }
 };
--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,7 @@
-var extend = function() {
+var extend = function(destination,source) {
+
+  for (var prop in source) {
+    destination[prop] = source[prop]
+  }
+    return destination
 };
