333df3c0975125e1b06be5982cddd1c4346dd192
Christopher Palmer <snly2386@gmail.com>
2014-04-15 10:17:47 -0500

    Finish Template

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,13 @@
-var render = function(){
+var render = function(template,object){
+  // newray = []
+  for(var prop in object) {
+    // newray.push(object[prop])
+    // template = "<p>{{firstName}}</p>"
+    // data = { firstName: "Chris",
+    //         lastName: "Palmer" }
 
+    // var replace = new RegExp ( "{{" + prop + "}}",'g')
+    template = template.replace( new RegExp ( "{{" + prop + "}}",'g'),object[prop])
+    }
+  return template;
 };
