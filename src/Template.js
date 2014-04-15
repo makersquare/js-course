c46698955d3aee5df52904d40b7dc069c5c2a6d4
Brady Bryan <jbraden.bryan@gmail.com>
2014-04-15 10:15:14 -0500

    replaces all the thingies

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,9 @@
-var render = function(){
-
+var render = function(template, data){
+  // var re = new RegExp(template)
+  var rtemp = template;
+  for(var prop in data){
+    var re = new RegExp("{{"+prop+"}}","g")
+    rtemp = rtemp.replace(re, data[prop])
+  }
+  return rtemp
 };
