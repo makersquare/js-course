9f021ea335558bb5f33f8613ca4b5372d134c7a7
Christopher Palmer <snly2386@gmail.com>
2014-04-16 10:07:02 -0500

    Add skycon feature

--- src/Template.js
+++ src/Template.js
@@ -1,13 +1,8 @@
 var render = function(template,object){
-  // newray = []
+  
   for(var prop in object) {
-    // newray.push(object[prop])
-    // template = "<p>{{firstName}}</p>"
-    // data = { firstName: "Chris",
-    //         lastName: "Palmer" }
-
-    // var replace = new RegExp ( "{{" + prop + "}}",'g')
     template = template.replace( new RegExp ( "{{" + prop + "}}",'g'),object[prop])
     }
   return template;
 };
+
