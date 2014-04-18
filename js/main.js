bb4b767cd90f0c03b605d566f4c7d82d3606d284
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-18 08:15:39 -0500

    robin template, finished, go back and refactor

--- js/main.js
+++ js/main.js
@@ -10,18 +10,35 @@
 // $('#store').append(newHtml);
 
 
-var items = [
-  { name: 'strawberry', price: 2.99 },
-  { name: 'orange', price: 3.99 },
-  { name: 'coconut', price: 4.99}
-];
+// var items = [
+//   { name: 'strawberry', price: 2.99 },
+//   { name: 'orange', price: 3.99 },
+//   { name: 'coconut', price: 4.99}
+// ];
 
-// above is an array with objects at each index
+// // above is an array with objects at each index
 
 
-var itemTemplate = $('#templates .item').html();
+// var itemTemplate = $('#templates .item').html();
 
-for (var i =0; i <items.length;i++) {
-  var newHtml = Robin.render(itemTemplate,items[i])
-  $('#store').append(newHtml);
-}
+// for (var i =0; i <items.length;i++) {
+//   var newHtml = Robin.render(itemTemplate,items[i])
+//   $('#store').append(newHtml);
+// }
+
+$("form").on("submit",function(e) {
+  e.preventDefault();
+  // need to save values on submit
+  // var items = {};
+  var foodname = $(".food").val();
+  // items['name'] = foodname
+  var priceamount = $(".price").val();
+  // items['price'] = priceamount
+  console.log(foodname);
+
+  var items = { "name": foodname, "price": priceamount};
+  alert(items);
+  var itemTemplate = $('#templates .item').html();
+  var newHTML = Robin.render(itemTemplate,items)
+  $('#store').append(newHTML)
+});
