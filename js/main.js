779c0beaf2601c8adba7c4da75c5e3c81587088d
Jose R. Borja <chakaitos@gmail.com>
2014-04-17 20:20:59 -0500

    Implement functionality to render videos from an existing list and also for a user to input values and create a new video

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
+var videoTemplate = $('#templates .video-list-item').html();
 
 var videos = [
   { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
   { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
 ];
 
+// loops the videos object and renders a template for each video.
+// then it appends the template to the html. 
+// finally the (); at the end automatically runs the function so the list is updated
 var renderVideoList = function () {
-  // TODO
-};
+	for (var video in videos) {
+		newItem = Robin.render(videoTemplate, videos[video])
+		$('#video-list').append(newItem);
+	}
+}();
+
+// Grabs the user inputs and renders a template for the values added(new video)
+// Then it appends the template to the html for display to user
+$('#new-video').on('submit', function(e) {
+  e.preventDefault();
+  var video = {
+    title: $('#video-title').val(),
+    youtubeId: $('#youtube-id').val()
+  }
+
+  var newVideo = Robin.render(videoTemplate, video);
+  $('#video-list').append(newVideo);
+});
