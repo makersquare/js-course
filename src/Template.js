6fb8977cedf7215d1fd2104bae0385eeabe9f058
Canaan Davis <canaandavis@canaans-mbp.att.net>
2014-05-30 11:15:30 -0500

    complete assignment

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,7 @@
-var render = function(){
-
+var render = function(template, data){
+  for (var i in data) {
+    var match = RegExp("{{" + i + "}}", "g");
+    template = template.replace(match, data[i]);
+  }
+  return template;
 };
