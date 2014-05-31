99b3764589a6f13814f30a6f7bd7bad2feab2ae0
Benjamin Poon <benjamintpoon@gmail.com>
2014-05-31 17:35:41 -0500

    implement extend function, passes tests

--- src/Extend.js
+++ src/Extend.js
@@ -1,2 +1,6 @@
-var extend = function() {
+var extend = function(destination, source) {
+	Object.keys(source).forEach(function(prop) {
+		destination[prop] = source[prop];
+	})
+	return destination;
 };
