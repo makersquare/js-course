eb83574c8b4ed71c72765a1db6ee5a33862a5636
Amy Bakameyer <bymak87@gmail.com>
2014-06-01 01:07:03 -0500

    passes all tests

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,8 @@
-var render = function(){
-
+var render = function(temp, data){
+for (var x in data) {
+    var match = RegExp("{{" + x + "}}","g");
+    temp = temp.replace(match, data[x]);
+  }
+  return temp;
 };
+
