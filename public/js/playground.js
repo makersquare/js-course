26d07a1cb509edd3c99fed22906468ba2b3da445
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-06-10 10:20:06 -0500

    Level 1 - properly closured i

--- public/js/playground.js
+++ public/js/playground.js
@@ -3,11 +3,13 @@ for (var i = 0; i < endpointCount; i++) {
 	// Make a call to our server, requesting a set of data.
 	// The data we get back is a chunk of strings that form
 	// a larger message.
-	$.ajax({
-		type: 'GET',
-		url: '/' + i,
-		success: function() {
-			console.log('Got back success from call ' + i + '!');
-		}
-	});
+	(function(i) {
+		$.ajax({
+			type: 'GET',
+			url: '/' + i,
+			success: function() {
+				console.log('Got back success from call ' + i + '!');
+			}
+		});
+	})(i);
 }
