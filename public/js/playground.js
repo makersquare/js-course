bdc0c057f4e9b53718d43227ab3a0b0070ffbc1f
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-08-27 09:52:00 -0500

    Added level 3 solution

--- public/js/playground.js
+++ public/js/playground.js
@@ -1,6 +1,6 @@
 var callbackObjects = [];
 var allDone = false;
-function checkCompletion(callback) {
+function checkCompletion(callback, reverse) {
 	// Make sure we don't run this multiple times once we're done
 	if (allDone) return;
 	for (var i in callbackObjects) {
@@ -8,11 +8,14 @@ function checkCompletion(callback) {
 		if (!callbackObjects[i].done) return false;
 	}
 	allDone = true;
-	var data = sortAndReturnData();
+	var data = sortAndReturnData(reverse);
 	callback(data);
+	// Reset for next run
+	callbackObjects = [];
+	allDone = false;
 }
 
-function retrieveData(callback) {
+function retrieveData(callback, reverse) {
 	var endpointCount = 77;
 	for (var i = 0; i < endpointCount; i++) {
 		// Make a call to our server, requesting a set of data.
@@ -31,18 +34,24 @@ function retrieveData(callback) {
 					callbackObjects[i].done = true;
 					callbackObjects[i].data = JSON.parse(response);
 					// Check whether or not we've finished
-					checkCompletion(callback);
+					checkCompletion(callback, reverse);
 				}
 			});
 		})(i);
 	}
 }
 
-function sortAndReturnData() {
+function sortAndReturnData(reverse) {
 	console.log('Sorting and returning');
-	callbackObjects.sort(function(a, b) {
-		return a.id - b.id;
-	});
+	if (reverse) {
+		callbackObjects.sort(function(a, b) {
+			return b.id - a.id;
+		});
+	} else {
+		callbackObjects.sort(function(a, b) {
+			return a.id - b.id;
+		});
+	}
 	var finalString = '';
 	for (var i in callbackObjects) {
 		console.log(callbackObjects[i])
@@ -53,6 +62,18 @@ function sortAndReturnData() {
 	return finalString.replace(/\n/g, "<br />");
 }
 
+// Retrieve data
 retrieveData(function(data) {
+	// Append data
 	$('#container').append(data);
+	// Add a little space... don't use BRs like me
+	$('#container').append('<br><br>');
+	// Wait 3 seconds
+	setTimeout(function() {
+		// Retrieve data again
+		retrieveData(function(secondData) {
+			// Append again
+			$('#container').append(secondData);
+		}, true); // <-- reverse = true
+	}, 3000)
 });
