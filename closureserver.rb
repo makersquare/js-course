125a1fdad9a8abb20ebce5d4786ca3feb515db4a
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-08-27 08:34:42 -0500

    Added bind call

--- closureserver.rb
+++ closureserver.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'json'
 
+# Comment this out if you're not using Vagrant.
+set :bind, '10.10.10.10'
+
 string = "Whosever room this is should be ashamed!\n"\
 		"His underwear is hanging on the lamp.\n"\
 		"His raincoat is there in the overstuffed chair,\n"\
