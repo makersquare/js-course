aa0e03747749d3a11e18e4e4bd7f3fd5a510a95f
Shehzan Devani <shehzandevani@gmail.com>
2014-09-12 13:07:46 -0700

    Starting checkers

---
+++ src/checkers.js
@@ -0,0 +1,24 @@
+var board = [
+  ['red', null, 'red', null, 'red', null, 'red', null],
+  [null, 'red', null, 'red', null, 'red', null, 'red'],
+  ['red', null, 'red', null, 'red', null, 'red', null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, 'blk', null, 'blk', null, 'blk', null, 'blk'],
+  ['blk', null, 'blk', null, 'blk', null, 'blk', null],
+  [null, 'blk', null, 'blk', null, 'blk', null, 'blk']
+];
+
+var attemptMove = function (position1, position2) {
+};
+
+// Checkers
+// Board has changed
+// Move has been made
+// Piece has been taken
+// An invalid move has been attempted
+// It is the next player's turn
+// A piece has been 'kinged'
+// It is game over
+// It is stalemate
+// There is potential for a double take
