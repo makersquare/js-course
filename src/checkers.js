c14fe2cbb788f9585b9c0fa95863bc396b0052c8
Shehzan Devani <shehzandevani@gmail.com>
2014-09-12 15:20:13 -0700

    Basic moves can be made

--- src/checkers.js
+++ src/checkers.js
@@ -1,16 +1,61 @@
 var board = [
-  ['red', null, 'red', null, 'red', null, 'red', null],
-  [null, 'red', null, 'red', null, 'red', null, 'red'],
-  ['red', null, 'red', null, 'red', null, 'red', null],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  [null, 'blk', null, 'blk', null, 'blk', null, 'blk'],
-  ['blk', null, 'blk', null, 'blk', null, 'blk', null],
-  [null, 'blk', null, 'blk', null, 'blk', null, 'blk']
+  ['red', ' X ', 'red', ' X ', 'red', ' X ', 'red', ' X '],
+  [' X ', 'red', ' X ', 'red', ' X ', 'red', ' X ', 'red'],
+  ['red', ' X ', 'red', ' X ', 'red', ' X ', 'red', ' X '],
+  [' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X '],
+  [' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X '],
+  [' X ', 'blk', ' X ', 'blk', ' X ', 'blk', ' X ', 'blk'],
+  ['blk', ' X ', 'blk', ' X ', 'blk', ' X ', 'blk', ' X '],
+  [' X ', 'blk', ' X ', 'blk', ' X ', 'blk', ' X ', 'blk']
 ];
 
-var attemptMove = function (position1, position2) {
+var currentPlayer = 'blk';
+
+var attemptMove = function (row1, col1, row2, col2) {
+  if (board[row1][col1] == ' X ') {
+    $(document).trigger('invalidMove', "What are you trying to move?");
+  } elsif (board[row1][col1] != currentPlayer) {
+    $(document).trigger('invalidMove', "That's not your piece");
+  }
+
+  if (board[row2][col2] != ' X ') {
+    $(document).trigger('invalidMove', "There's already a piece at the destination"); 
+  }
+
+  var direction = (currentPlayer == 'red') ? 1 : -1;
+  var enemy = (currentPlayer == 'red') ? 'blk' : 'red';
+
+  if (row1 + direction == row2 && (col1 + 1 == col2 || col1 - 1 == col2)) {
+    makeMove(row1, col1, row2, col2);
+  } else if (row1 + direction * 2 == row2 &&
+      (col1 + 2 == col2 || col1 - 2 == col2)) {
+    var midRow = row1 + direction;
+    var midCol = col1 + (col2 - col1) / 2;
+    if (board[midRow][midCol] == enemy) {
+      makeMove(row1, col1, row2, col2);
+      removePiece(midRow, midCol);
+    } else {
+      $(document).trigger('invalidMove', "That's just not a valid move");   
+    }
+    // call proper methods
+  } else {
+    $(document).trigger('invalidMove', "That's just not a valid move");
+  }
 };
+
+var makeMove = function(row1, col1, row2, col2) {
+  var piece = board[row1][col1];
+  board[row1][col1] = ' X ';
+  board[row2][col2] = piece;
+  currentPlayer = currentPlayer == 'red' ? 'blk' : 'red';
+  $(document).trigger('moveMade', currentPlayer, row1, col1, row2, col2);
+}
+
+var removePiece = function(row, col) {
+  var enemy = board[row][col];
+  board[row][col] = ' X ';
+  $(document).trigger('pieceTaken', currentPlayer, enemy, row, col);
+}
 
 // Checkers
 // Board has changed
