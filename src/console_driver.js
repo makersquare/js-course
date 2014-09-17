fd519dd1df2508fc6324cdb6150a5ba4c0e8814c
Shehzan Devani <shehzandevani@gmail.com>
2014-09-16 23:53:16 -0500

    Setting up board for starting position

--- src/console_driver.js
+++ src/console_driver.js
@@ -10,22 +10,6 @@ var charToNum = {
   h: 7
 }
 
-var displayError = function (e, error) {
-  console.log(error);
-};
-
-var play = function () {
-  resetBoard();
-
-  while (true) {
-    var move = getMove();
-    if (move.quit) {
-      break;
-    }
-    attemptMove(move.startRow, move.startCol, move.endRow, move.endCol);
-  }
-}
-
 var displayBoard = function () {
   var column = [0, 1, 2, 3, 4, 5, 6, 7];
   console.log("  | " + column.join("   "));
@@ -34,29 +18,3 @@ var displayBoard = function () {
     console.log(numToChar[i] + " |" + board[i].join(" "));
   }
 };
-
-var getMove = function () {
-  var start = prompt("Pick starting piece position (example: f5)");
-  if (start[0] == "q") {
-    return {
-      quit: true
-    }
-  }
-  var end = prompt("Pick ending position");
-  start = start.split("")
-  end = end.split("")
-  return {
-    startRow: charToNum[start[0]],
-    startCol: Number(start[1]),
-    endRow: charToNum[end[0]],
-    endCol: Number(end[1])
-  }
-};
-
-var taunt = function () {
-  console.log("Piece Taken! HAHA");
-}
-
-$(document).on('invalidMove', displayError);
-$(document).on('boardChange', displayBoard);
-$(document).on('pieceTaken', taunt);
