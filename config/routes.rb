c68379ec92d27fb260be204b0ac02ac587c74ea4
Shehzan <shehzan@makersquare.com>
2014-10-20 01:34:20 -0700

    Adding cross reference verification and default resources are json

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders, defaults: {format: :json}
 
   root 'items#home'
-  resources :items
+  resources :items, defaults: {format: :json}
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
