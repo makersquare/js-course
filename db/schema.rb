9e34c31750a3209aa944528a44eb50483215620c
Dudemullet <elpaip@gmail.com>
2014-10-26 08:28:14 -0700

    Migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141020073312) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: true do |t|
     t.string   "name"
     t.text     "description"
