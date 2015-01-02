4f97d247faa7d8b64232714bc81c70e75c515e82
Fernando Catacora <nando.drw@gmail.com>
2015-01-02 13:14:33 -0600

    processing evidences

--- src/investigation.js
+++ src/investigation.js
@@ -22,9 +22,60 @@
     });
 
     caseDetails.then(function(data){
-      console.log('data',data);
+
+      var arrayOfPromises = [];
+
+      for(var i in data.evidences){
+        arrayOfPromises.push(new Promise(function(resolve,reject){
+
+          var requestUrl = '';
+          var requestData = { caseId : data.id};
+
+          switch (data.evidences[i].evidenceType) {
+            case 'witness' :
+              requestUrl = '/FBI/API/interview';
+              requestData.who = data.evidences[i].evidenceDescription;
+              break;
+            case 'items' :
+              requestUrl = '/FBI/API/itemLaboratory';
+              requestData.what = data.evidences[i].evidenceDescription;
+              break;
+            case 'injuries' :
+              requestUrl = '/FBI/API/medicalAnalysis';
+              requestData.what = data.evidences[i].evidenceDescription;
+              break;
+            case 'media' :
+              requestUrl = '/FBI/API/mediaLaboratory';
+              requestData.what = data.evidences[i].evidenceDescription;
+              break;
+          };
+
+          $.ajax({
+            url : requestUrl,
+            data : requestData,
+            type : 'POST',
+            dataType : 'json',
+            success : function (data){
+              resolve(data);
+            },
+            error : function(err){
+              reject(err);
+            }
+          });
+
+        }));
+      };
+
+      return Promise.all(arrayOfPromises);
+
     },function(err){
-      console.log('err',err);
+      console.log('first promise fail',err);
+    }).
+
+    then(function(responses){
+      console.log('responses',responses);
+    },function(err){
+      console.log('first array of promises fail',err);
     });
 
     result.textContent = 'he or she still being unknown :('
