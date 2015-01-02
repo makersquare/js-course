5329553146451cf584dec02ae12654334473d6ec
Fernando Catacora <nando.drw@gmail.com>
2015-01-02 17:23:04 -0600

    get suspects hash

--- src/investigation.js
+++ src/investigation.js
@@ -7,6 +7,11 @@
 
     // your code goes here
 
+    // auxiliar variables
+    var caseId = undefined;
+    var analysisResults = undefined;
+    var suspectsHash = undefined;
+
     var caseDetails = new Promise(function(resolve,reject){
       $.ajax({
         url : '/FBI/API/case',
@@ -23,13 +28,16 @@
 
     caseDetails.then(function(data){
 
+      // filling suxilisr variables
+      caseId = data.id;
+
       var arrayOfPromises = [];
 
       for(var i in data.evidences){
         arrayOfPromises.push(new Promise(function(resolve,reject){
 
           var requestUrl = '';
-          var requestData = { caseId : data.id};
+          var requestData = { caseId : caseId};
 
           switch (data.evidences[i].evidenceType) {
             case 'witness' :
@@ -66,6 +74,7 @@
         }));
       };
 
+
       return Promise.all(arrayOfPromises);
 
     },function(err){
@@ -73,9 +82,75 @@
     }).
 
     then(function(responses){
-      console.log('responses',responses);
+
+      // filling suxilisr variables
+      analysisResults = responses;
+
+      var arrayOfPromises = [];
+
+      for(var i in responses){
+
+        var requestData = {
+          caseId : caseId,
+          characteristics : responses[i].conclusion
+        };
+
+        arrayOfPromises.push(new Promise(function(resolve,reject){
+            $.ajax({
+              url : '/FBI/API/suspectsList',
+              data : requestData,
+              type : 'GET',
+              dataType : 'json',
+              success : function (data){
+                resolve(data);
+              },
+              error : function(err){
+                reject(err);
+              }
+            });
+        }));
+      };
+
+      return Promise.all(arrayOfPromises);
+
     },function(err){
-      console.log('first array of promises fail',err);
+      console.log('first array of promises fails',err);
+    }).
+
+    then(function(responses){
+
+      // since for each element in 'analysis Results'
+      // we have a corresponding element on
+      // the 'responses' array, we can do the following:
+
+      // filling suxilisr variables
+      suspectsHash = {};
+
+      for(var i in responses){
+        for(var j in responses[i]){
+          if(suspectsHash[responses[i][j].s_id]){
+            suspectsHash[responses[i][j].s_id].count++;
+            suspectsHash[responses[i][j].s_id].evidences.push({
+              accuracy : analysisResults[i].accuracy,
+              weight : analysisResults[i].weight
+            });
+          } else {
+            suspectsHash[responses[i][j].s_id] = {
+              count : 1,
+              name : responses[i][j].name,
+              evidences : [{
+                accuracy : analysisResults[i].accuracy,
+                weight : analysisResults[i].weight
+              }]
+            };
+          }
+        };
+      };
+
+      console.log('Suspect hash',suspectsHash);
+
+    },function(err){
+      console.log('second array of promises fails',err);
     });
 
     result.textContent = 'he or she still being unknown :('
@@ -83,4 +158,20 @@
   };
 
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
