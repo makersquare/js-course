b2978a7a70ac406a34cec912264469b3367a94df
Fernando Catacora <nando.drw@gmail.com>
2015-01-02 12:32:42 -0600

    get case details

--- src/investigation.js
+++ src/investigation.js
@@ -7,6 +7,26 @@
 
     // your code goes here
 
+    var caseDetails = new Promise(function(resolve,reject){
+      $.ajax({
+        url : '/FBI/API/case',
+        type : 'GET',
+        dataType : 'json',
+        success : function (data){
+          resolve(data);
+        },
+        error : function(err){
+          reject(err);
+        }
+      });
+    });
+
+    caseDetails.then(function(data){
+      console.log('data',data);
+    },function(err){
+      console.log('err',err);
+    });
+
     result.textContent = 'he or she still being unknown :('
 
   };
