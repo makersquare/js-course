7adc04af644f5f4525d08d721d6b35d1a1dca865
Julia Baritz <jbaritz@gmail.com>
2015-01-04 21:10:59 -0600

    display moves on board

--- src/UI.js
+++ src/UI.js
@@ -2,7 +2,15 @@ $(document).ready(function (){
 
 	$('.start').on('click',function(){
 		resetBoard();
-	})
+	});
+
+	$('.col').on('click',function(){
+		var col = this.className;
+		var colx = col.slice(-1);
+		var row = $(this).parent('span').attr("class");
+		var rowx = row.slice(-1);
+		selectSquare(rowx,colx);
+	});
 
 	$(document).on('boardChange', function(){
 		displayBoard(); //just for console
@@ -14,10 +22,12 @@ $(document).ready(function (){
 		 	  $(row).find(col).empty();
 		 	}
 		 	else if (board[i][j] === "wht"){
+		 	  $(row).find(col).empty();
 		 	  var div = $('<div>').addClass('white piece');
 		 	  $(row).find(col).append(div);
 		 	}
 		 	else if (board[i][j] === "red"){
+		 	  $(row).find(col).empty();
 		 	  var div = $('<div>').addClass('red piece');
 		 	  $(row).find(col).append(div);
 		 	}
