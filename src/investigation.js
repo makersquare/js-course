4703b2b54245a48e4932562c149a634b098bfe2f
Fernando Catacora <nando.drw@gmail.com>
2015-01-07 16:29:28 -0600

    start point

---
+++ src/investigation.js
@@ -0,0 +1,15 @@
+(function(){
+
+  var invetigateButton = document.querySelector('button');
+  var result = document.querySelector('h3');
+
+  invetigateButton.onclick = function (event){
+
+    // your code goes here
+
+
+
+    result.textContent = 'he or she still being unknown  :('
+  };
+
+})();
