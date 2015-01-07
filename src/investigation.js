6a80634e68a2719b0b79c83d9ae4d3141748820e
Fernando Catacora <nando.drw@gmail.com>
2015-01-07 16:33:17 -0600

    get case details

--- src/investigation.js
+++ src/investigation.js
@@ -7,7 +7,10 @@
 
     // your code goes here
 
-
+    $.get('/FBI/API/case',function(data){
+      var caseDetail = data;
+      console.log('case details:',data);
+    },'json');
 
     result.textContent = 'he or she still being unknown  :('
   };
